ccb85d9dfa392c2a997fc9be2c7a02f9872e0562
misazhu <misa.zhu@gmail.com>
2026-03-10 01:02:34 +0800

    remove https_test

--- apps/https_test/include/netdb.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXTRA_APPS_HTTPS_TEST_NETDB_H
-#define EXTRA_APPS_HTTPS_TEST_NETDB_H
-
-#include <sys/socket.h>
-
-const char* ewok_gai_strerror_compat(int ecode);
-void ewok_freeaddrinfo_compat(struct addrinfo *res);
-
-#endif
